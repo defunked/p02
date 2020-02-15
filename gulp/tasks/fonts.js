9b7209a4695212f93790c1f419d12067e8fc4522
Aleksandr Grigoriev <a.greegoriev@gmail.com>
2020-02-16 01:24:18 +0300

    Подготовка вёрстки второго этапа для gh pages

--- gulp/tasks/fonts.js
+++
@@ -1,6 +0,0 @@
-module.exports = function() {
-    $.gulp.task('fonts', () => {
-        return $.gulp.src('./app/resources/fonts/**/*.*')
-            .pipe($.gulp.dest('./dist/assets/fonts/'));
-    });
-};
--- gulp/tasks/html.js
+++
@@ -1,7 +0,0 @@
-module.exports = function() {
-    $.gulp.task('html', () => {
-        return $.gulp.src('./app/pages/*.html')
-            .pipe($.gulp.dest('./dist/'))
-            .on('end', $.browserSync.reload);
-    });
-};
